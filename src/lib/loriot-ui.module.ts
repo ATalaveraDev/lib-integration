a292ffa5724b835aadd99a4affdca8852de921d4
Angel Talavera <ata@loriot.io>
2019-06-10 12:10:53 +0200

    First commit

---
+++ src/lib/loriot-ui.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { LoriotUiComponent } from './loriot-ui.component';
+
+@NgModule({
+  declarations: [LoriotUiComponent],
+  imports: [
+  ],
+  exports: [LoriotUiComponent]
+})
+export class LoriotUiModule { }
