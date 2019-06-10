ef99be07c12b5f21f2bd1a8d2a8bfeed88c940f9
Angel Talavera <ata@loriot.io>
2019-06-10 11:56:10 +0200

    Blue butto

---
+++ projects/loriot-ui/button/blue/blue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lrt-blue',
+  templateUrl: './blue.component.html',
+  styleUrls: ['./blue.component.css']
+})
+export class BlueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/loriot-ui/button/button.module.ts
+++ projects/loriot-ui/button/button.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BlueComponent } from './blue/blue.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [BlueComponent],
   imports: [
     CommonModule
   ]
